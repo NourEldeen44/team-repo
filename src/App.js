77a360f818361cb86c9cd77899000c5672089b49
Alaa-Ezzat <alaa.awadallah97@gmail.com>
2022-06-28 17:48:51 +0200

    Alaa Update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Alaa from "./Alaa/Alaa";
 
 function App() {
   return (
     <div className="App">
-      {/* Please Add your name in a h1 here */}
-      {/* 1- */}
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Alaa />
     </div>
   );
 }
