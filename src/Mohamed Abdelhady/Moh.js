d2bfdaacd5325e97341ad315a63f1ea3adfd8a8f
Mohamed Eldamaty <damaty2000@gmail.com>
2022-06-29 17:06:30 +0200

    Mohamed Abdelhady Component

---
+++ src/Mohamed Abdelhady/Moh.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Moh = () => {
+    return (
+        <div>
+            <h1>My Name is Mohamed Abdelhady</h1>
+            <p>Thank u iti.</p>
+        </div>
+    );
+}
+
+export default Moh;
